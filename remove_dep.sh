f47be00b2bc39370053ef4cc5ff1d8c0ac304e6f
Csome <csomee@qq.com>
2025-03-25 14:41:31 +0800

    fix: streamline package removal in remove_dep.sh by reducing listed packages

--- remove_dep.sh
+++ remove_dep.sh
@@ -2,20 +2,8 @@
 
 echo "WARNING: This script will remove several packages and their dependencies from your system."
 echo "The following packages will be removed:"
-echo "  - git"
-echo "  - python-is-python3"
 echo "  - tmux"
 echo "  - python3-venv"
-echo "  - gcc"
-echo "  - gdb"
-echo "  - gdbserver"
-echo "  - python3-dev"
-echo "  - python3-setuptools"
-echo "  - libglib2.0-dev"
-echo "  - libc6-dbg"
-echo "  - curl"
-echo "  - libc6-dbg:i386"
-echo "  - libgcc-s1:i386"
 echo "This action cannot be undone. Are you sure you want to proceed? (yes/no)"
 read -r confirmation
 if [[ "$confirmation" != "yes" ]]; then
@@ -27,20 +15,8 @@ set -x
 
 # Remove apt packages installed by install.sh and pwndbg setup
 sudo apt-get remove --purge \
-  git \
-  python-is-python3 \
   tmux \
-  python3-venv \
-  gcc \
-  gdb \
-  gdbserver \
-  python3-dev \
-  python3-setuptools \
-  libglib2.0-dev \
-  libc6-dbg \
-  curl \
-  libc6-dbg:i386 \
-  libgcc-s1:i386 || exit 1
+  python3-venv || exit 1
 
 # Clean up any unused dependencies and cached files
 sudo apt-get autoremove -y || exit 1
