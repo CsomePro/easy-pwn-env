a92417446951d53d498039e1cdc29854fdab6eff
Csome <csomee@qq.com>
2025-03-25 04:00:04 +0000

    add lazyvim option.lua

---
+++ options.lua
@@ -0,0 +1,5 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/lazyvim/lazyvim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+
+vim.opt.clipboard = "unnamedplus"
